9a94e551d7dc7b0750864507d7acb3f45666b714
Wanderson Honorato Araujo <07279901360@espcid.intra>
2026-04-08 21:14:38 -0300

    correção atividade condição

--- Modulo 1/Aula14/script.js
+++ Modulo 1/Aula14/script.js
@@ -5,50 +5,50 @@
 // Crie um programa que receba a idade e se a pessoa tem ingresso Ela só pode entrar se a idade for maior que 16 anos e tiver ingresso
 
 
-let idade = prompt('Qual sua idade?')
-let Ingresso = confirm('Você tem o ingresso?')
+// let idade = prompt('Qual sua idade?')
+// let Ingresso = confirm('Você tem o ingresso?')
 
-let AcessCinema = (idade > 16 && Ingresso) 
-      ? "Acesso permitido! Bom filme." 
-      : "Acesso negado. Verifique a idade ou o ingresso.";
-console.log(AcessCinema);
+// let AcessCinema = (idade > 16 && Ingresso) 
+//       ? "Acesso permitido! Bom filme." 
+//       : "Acesso negado. Verifique a idade ou o ingresso.";
+// console.log(AcessCinema);
 
 
 // 2. Validação de login:
 // Crie um programa que peça o nome de usuário e a senha, se o nome de usuário for 'admin' e a senha for 'admin123' diga que ela conseguiu logar se ela errar uma das duas diga 'usuário não encontrado'
 
 
-let user = prompt('Qual o seu usuário?')
-let pass = prompt('Qual a sua senha?')
+// let user = prompt('Qual o seu usuário?')
+// let pass = prompt('Qual a sua senha?')
 
-let statusAcesso  = (user === 'admin' && pass === 'admin123') 
-     ? "Login realizado com sucesso! (200)" 
-     : "Usuário não econtrado (404)";
-console.log(statusAcesso);
+// let statusAcesso  = (user === 'admin' && pass === 'admin123') 
+//      ? "Login realizado com sucesso! (200)" 
+//      : "Usuário não econtrado (404)";
+// console.log(statusAcesso);
 
 
 //  3. Frete grátis:
 // Crie um programa que receba o valor de uma compra e se o cliente é VIP. O cliente ganhará frete grátis se a compra for mais de 1000 ou se for VIP.
 
 
-let value = prompt('Qual o valor da sua compra?')
-let vip = confirm('Você é VIP?')
+// let value = prompt('Qual o valor da sua compra?')
+// let vip = confirm('Você é VIP?')
 
-let freeDelivery  = ((value > 1000) && (vip === true))
-        ? "Você ganhou frete gratis na sua compra" 
-        : "Você não é Vip e não atingiu o limite de compra";
-console.log(freeDelivery);
+// let freeDelivery  = ((value > 1000) && (vip === true))
+//         ? "Você ganhou frete gratis na sua compra" 
+//         : "Você não é Vip e não atingiu o limite de compra";
+// console.log(freeDelivery);
 
 
 
 // 4. Bloqueio de acesso:
 // Crie um programa que receba se o usuário digitou senha correta. Se a senha não estiver correta, mostrar "Acesso negado".
 
-let senha = prompt('Digite sua senha')
-senha = parseInt(senha)
+// let senha = prompt('Digite sua senha')
+// senha = parseInt(senha)
 
-let ErrorPasswaord = ((senha === 'admin123')) ? 'Logado com sucesso' : 'Acesso Negado'
-console.log(ErrorPasswaord);
+// let ErrorPasswaord = ((senha === 'admin123')) ? 'Logado com sucesso' : 'Acesso Negado'
+// console.log(ErrorPasswaord);
 
 
 // 5. Entrada em festa
@@ -185,7 +185,15 @@ console.log(ErrorPasswaord);
 // Solicite ao usuário uma opção digitando:
 // 1 - Cadastrar usuário
 // 2 - Listar usuários
-// 3 - Sair do sistema
+// 3 - Sair do sis
+// tema
+
+
+// let opcao = null 
+
+// while(opcao != '3'){
+//       opcao = prompt("Digite uma opção /n1 - Cadastrar")
+// } switch()
 
 
 
@@ -199,7 +207,31 @@ console.log(ErrorPasswaord);
 
 // 1. Crie um programa que seria uma lista de desejos, onde o usuário pode pedir 3 coisas e você mostra cada um dos pedidos na tela.
 
+// for(let i=0; 1 < 3; i ++){
+//       let desejo = prompt('Digite o primeiro item')
+// } console.log(`desejo`)
+
+
+
 // 2. Crie um programa que receba um valor de compra e depois um número de parcelas, e depois mostre na tela o valor de cada parcela utilizando um for
+
+let valorCompra = Number(prompt('Digite o valor da compra'))
+let numeroParcelas = parseInt(prompt('Digite o numero de parcelas'))
+
+let parcela = valorCompra/numeroParcelas
+
+for(let i=1; i <= numeroParcelas; i++){
+      console.log(`parcela ${1} = ${parcela.toFixed(2)}`)
+}
+
+// & as duas condição tem que ser V
+// || as duas condições tem que ser V e a outra F para entrar na While
+
+ while( (numeroParcelas >= 0) & (numeroParcelas <= 12) ) {
+      console.log('Valor inválido, o nºde parcelas é em até 12x')
+ }
+
+
 
 // 3. Um sistema de caixa precisa calcular o total de uma compra.
 
@@ -210,9 +242,23 @@ console.log(ErrorPasswaord);
 
 // Ao final, exiba o total da compra.
 
+let quantidadeProdutos = parseInt(prompt('Qual a quantidade de produtos comprados?'))
+let preçoProduto = parseFloat(prompt('Digite o valor do produto'))
+
+let totalCompras = 0
+
+for (let i =0; i < quantidadeProdutos; i++){
+      console.log("O total da compra é: R$ " + totalCompras.toFixed(2));
+      totalCompras += preçoProduto;
+}
+
+
+
 // 4. Um professor tem 10 alunos e precisa saber quantos foram aprovados, crie um programa que peça a nota de cada um dos 10 alunos e se a nota for maior ou igual a 7, mostre aprovado, se não, recuperação.
 
 // no final mostre o total de alunos aprovados e de recuperação
+
+
 
 
 // #############################
